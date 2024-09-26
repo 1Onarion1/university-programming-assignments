c55c682f7e6f8979b240c83c2d009d5c186a6e39
1Onarion1 <danil.boronenkov.02@bk.ru>
2024-09-26 22:56:08 +0300

    update readme/main

--- module1/week1/max-min-values/src/main.cpp
+++ module1/week1/max-min-values/src/main.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
-#include <limits>
+#include <limits> // Подключаем библиотеку для использования std::numeric_limits 
 
-
-void printDataLimits() {
-	std::cout << "Type int:\n";
-	std::cout << "Min: " << std::numeric_limits<int>::min() << "\n\n";
-	std::cout << "Max: " << std::numeric_limits<int>::max() << "\n\n";
-	std::cout << "Type unsigned int:\n";
-	std::cout << "Min: " << std::numeric_limits<unsigned int>::min() << "\n\n";
-	std::cout << "Max: " << std::numeric_limits<unsigned int>::max() << "\n\n";
-	std::cout << "Type short:\n";
-	std::cout << "Min: " << std::numeric_limits<short>::min() << "\n\n";
-	std::cout << "Max: " << std::numeric_limits<short>::max() << "\n\n";
-	std::cout << "Type unsigned short:\n";
-        std::cout << "Min: " << std::numeric_limits<unsigned short>::min() << "\n\n";
-        std::cout << "Max: " << std::numeric_limits<unsigned short>::max() << "\n\n";
-	std::cout << "Type long:\n";
-        std::cout << "Min: " << std::numeric_limits<long>::min() << "\n\n";
-        std::cout << "Max: " << std::numeric_limits<long>::max() << "\n\n";
-	std::cout << "Type float:\n";
-        std::cout << "Min: " << std::numeric_limits<float>::min() << "\n\n";
-        std::cout << "Max: " << std::numeric_limits<float>::max() << "\n\n";
-	std::cout << "Type double:\n";
-        std::cout << "Min: " << std::numeric_limits<double>::min() << "\n\n";
-        std::cout << "Max: " << std::numeric_limits<double>::max() << "\n\n";
-}
 
 int main() {
-	printDataLimits();
+	// Выводим минимальные и максимальные значения для различных типов данных
+
+
+	// Для типа int 
+	std::cout << "Тип int" std::endl;
+	std::cout << "Минимальное значение: " << std::numeric_limits<int>::min() << std::endl; // -2147483648
+	std::cout << "Максимальное значение: " << std::numeric_limits<int>::max() <<std::endl; //  2147483648
+	// int занимает 4 байта и используется для хранения целых значений.
+
+	// Для типа unsigned int
+	std::cout << "Тип: unsigned int" << std::endl;
+	std::cout << "Минимальное значение: " << std::numeric_limits<unsigned int>::min() << std::endl; // 0
+	std::cout << "Максимальное значение: " << std::numeric_limits<unsigned int>::max() << std::endl; // 4294967295
+	// unsigned int используется для хранения неотрицательных целых значений и занимает 4 байта.
+
+	// Для типа float
+	std::cout << "Тип: float" << std::endl;
+        std::cout << "Минимальное значение: " << std::numeric_limits<float>::min() << std::endl; // -3.40282e+38
+        std::cout << "Максимальное значение: " << std::numeric_limits<float>::max() << std::endl; // 3.40282e+38
+	// float занимает 4 байта и используется для хранения чисел с плавающей точкой.
+
+	// Для типа double
+	std::cout << "Тип: double" << std::endl;
+        std::cout << "Минимальное значение: " << std::numeric_limits<double>::min() << std::endl; // -1.79769е+308
+        std::cout << "Максимальное значение: " << std::numeric_limits<double>::max() << std::endl; // 1.79769e+308
+	// double занимает 8 байт и используется для хранения чисел с двойной точностью.
 	return 0;
 }
